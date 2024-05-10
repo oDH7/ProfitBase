f8e51d74974fcfcf1c8e956d036dc08047fffbcf
Marcos Leal <125682117+Marcosleal555556@users.noreply.github.com>
2024-05-10 09:03:07 -0300

    app

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+// Tab Navigation
+import { NavigationContainer } from "@react-navigation/native"; // Importa o componente NavigationContainer de "@react-navigation/native" para envolver a navegação da aplicação.
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// Páginas
+import Home from "./src/pages/Home";
+import Sobre from "./src/pages/Sobre";
+import CalcCripto from "./src/pages/CalcCripto";
+
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+// Cria uma instância de um navegador de pilha usando createNativeTabNavigator e a armazena na constante Tab.
+// Isso permite que você use esse navegador de pilha para definir a navegação em sua aplicação.
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* Componente fornecido pelo React Navigation para envolver a navegação da aplicação. */}
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarActiveTintColor: "#FF8800",
+          tabBarInactiveTintColor: "black",
+          tabBarStyle: {
+            backgroundColor: "#FFD7A9",
+            height: 50,
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            left: 20,
+            borderTopEndRadius: 15,
+            borderTopColor: "transparent",
+            padding: 10,
+            shadowColor: "white",
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0,
+            shadowRadius: 4.65,
+
+            elevation: 8,
+          },
+        }}
+      >
+        {/* Componente Navigator do navegador de pilha. initialRouteName define a rota inicial da navegação. */}
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <FontAwesome size={25} color={color} name="home" />;
+            },
+            headerShown: false,
+            title: "",
+          }}
+        />
+        {/* Define uma tela com o nome "Home" e associa o componente Home a ela. */}
+        <Tab.Screen
+          name="Sobre"
+          component={Sobre}
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <FontAwesome size={25} color={color} name="newspaper-o" />;
+            },
+            headerShown: false,
+            title: "",
+          }}
+        />
+        {/* Define uma tela com o nome "Sobre" e associa o componente Sobre a ela. */}
+        <Tab.Screen
+          name="CalcCripto"
+          component={CalcCripto}
+          options={{
+            tabBarIcon: ({ color }) => {
+              return <AntDesign size={25} color={color} name="calculator" />;
+            },
+            headerShown: false,
+            title: "",
+          }}
+        />
+        {/* Define uma tela com o nome "CalcImc" e associa o componente CalcImc a ela. */}
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
