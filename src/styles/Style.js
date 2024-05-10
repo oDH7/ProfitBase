341fd387caa2a405aa6c5d8a960ba66881e6b9c6
Marcos Leal <125682117+Marcosleal555556@users.noreply.github.com>
2024-05-10 09:01:32 -0300

    up

---
+++ src/styles/Style.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFD7A9",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 200,
+  },
+  profitbase: {
+    width: 187,
+    height: 35,
+    resizeMode: "contain",
+    marginTop: 0, // Adiciona margem acima da imagem do header
+  },
+  button1: {
+    backgroundColor: "#FF8800",
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 30, // Adiciona margem entre os botões do menu
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  bitcoin: {
+    width: 54,
+    height: 50,
+    resizeMode: "contain",
+    marginTop: 30, // Adiciona margem acima das imagens das criptomoedas
+  },
+  ethereum: {
+    width: 54,
+    height: 50,
+    resizeMode: "contain",
+    marginTop: 30,
+  },
+  solana: {
+    width: 54,
+    height: 50,
+    resizeMode: "contain",
+    marginTop: 30,
+  },
+  cryptoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "40%",
+    marginBottom: 20,
+    marginTop: 50,
+  },
+});
