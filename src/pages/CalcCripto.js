3b208875adceab2ccd84c68f5b21feff16a6ab89
Marcos Leal <125682117+Marcosleal555556@users.noreply.github.com>
2024-05-10 09:00:59 -0300

    up

---
+++ src/pages/CalcCripto.js
@@ -0,0 +1,90 @@
+import { View, Text, TouchableOpacity, Modal } from "react-native";
+import { styles } from "../styles/Style";
+
+// Import Hook useState
+import React, { useState } from "react";
+
+// Import Componentes
+import TxtInputComponent from "../components/TxtInputComponent";
+import { TextInput } from "react-native-web";
+
+export default function CalcCripto() {
+  const [valordecompra, setValordecompra] = useState("");
+  const [investimento, setInvestimento] = useState("");
+  const [valordevenda, setValordevenda] = useState("");
+  const [taxadecompra, setTaxadecompra] = useState("");
+  const [taxadevenda, setTaxadevenda] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [resultado, setResultado] = useState(0);
+
+  console.log("Resultado: ", resultado);
+
+  const profit_loss = () => {
+    const calcProfitLoss =
+      investimento * (valordevenda / valordecompra) -
+      taxadecompra -
+      taxadevenda -
+      investimento;
+    setResultado(calcProfitLoss); // Armazenando o resultado do cálculo
+    setVisible(true);
+  };
+
+  const calcularNovamente = () => {
+    setValordecompra("");
+    setInvestimento("");
+    setValordevenda("");
+    setTaxadecompra("");
+    setTaxadevenda("");
+    setVisible(false);
+
+    console.log("Investimento zerado: ", investimento);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Calcular ganhos/perdas</Text>
+      <TxtInputComponent
+        txtplace="Preço de compra"
+        value={valordecompra}
+        changeText={setValordecompra}
+      />
+      <TxtInputComponent
+        txtplace="Investimento"
+        value={investimento}
+        changeText={setInvestimento}
+      />
+
+      <TxtInputComponent
+        txtplace="Preçp de venda"
+        value={valordevenda}
+        changeText={setValordevenda}
+      />
+
+      <TxtInputComponent
+        txtplace="Taxa de compra"
+        value={taxadecompra}
+        changeText={setTaxadecompra}
+      />
+
+      <TxtInputComponent
+        txtplace="Taxa de venda"
+        value={taxadevenda}
+        changeText={setTaxadevenda}
+      />
+
+      <TouchableOpacity onPress={profit_loss}>
+        <Text>Calcular </Text>
+      </TouchableOpacity>
+
+      <Modal visible={visible}>
+        <View>
+          <Text>Modal</Text>
+          <TouchableOpacity onPress={calcularNovamente}>
+            <Text>Fechar</Text>
+          </TouchableOpacity>
+          <Text>Ganhos/Perdas :R${resultado}</Text>
+        </View>
+      </Modal>
+    </View>
+  );
+}
---
+++ src/pages/Sobre.js
@@ -0,0 +1,43 @@
+import { View, TouchableOpacity, Text, Image } from "react-native";
+import { styles } from "../styles/Style";
+
+// Import Componentes
+import TxtComponent from "../components/TextProps";
+
+// Import Hook useNavigation
+import { useNavigation } from "@react-navigation/native";
+
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export default function Sobre() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.touch} onPress={() => alert("Logo")}>
+        <Image
+          source={require("../uploads/black.png")}
+          resizeMode="contain"
+          style={styles.profitbase}
+        />
+        <Text>Sobre a Profit Base </Text>
+      </TouchableOpacity>
+      <TxtComponent title="Sobre" />
+      <Text style={styles.sobretext}>
+        O Profit Base é um aplicativo para investidores de criptomoedas e outros
+        ativos digitais. Ele oferece uma maneira simples de acompanhar o
+        desempenho de suas carteiras em tempo real, permitindo que você entenda
+        seus ganhos e tome decisões informadas para maximizar seus retornos.
+        Simplifique sua experiência de investimento com o Profit Base.
+      </Text>
+
+      <TouchableOpacity
+        style={styles.button1}
+        onPress={() => navigation.navigate("CalcCripto")}
+      >
+        <Text style={styles.buttonText}>Calcular Crypto</Text>
+      </TouchableOpacity>
+      <Text>Informações Autênticas</Text>
+      <Text>Dados reais e atualizados com base nos valores de mercado.</Text>
+    </View>
+  );
+}
